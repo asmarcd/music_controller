6e98809ded5cbb2a63fd2e7d1f2181e246413a64
Chris Asmar <asmarcd@gmail.com>
2025-02-13 17:59:25 -0600

    can create room

--- api/views.py
+++ api/views.py
@@ -6,9 +6,28 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 # Create your views here.
+
+
 class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
+
+class GetRoom(APIView):
+    serializer_class = RoomSerializer
+    lookup_url_kwarg = 'code'
+
+    def get(self, request, formatt=None):
+        code = request.GET.get(self.lookup_url_kwarg)
+        if code != None:
+            room = Room.objects.filter(code=code)
+            if len(room) > 0:
+                data = RoomSerializer(room[0]).data
+                data['is_host'] = self.request.session.session_key == room[0].host
+                return Response(data, status=status.HTTP_200_OK)
+            return Response({'Bad Request': 'Invalid Room Code'}, status.HTTP_404_NOT_FOUND)
+        return Response({'Bad Request': "code parameter not found in request"}, status.HTTP_404_NOT_FOUND)
+
 
 class CreateRoomView(APIView):
     serializer_class = CreateRoomSerializer
@@ -29,7 +48,8 @@ class CreateRoomView(APIView):
                 room.votes_to_skip = votes_to_skip
                 room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
             else:
-                room = Room(host=host, guest_can_pause=guest_can_pause, votes_to_skip=votes_to_skip)
+                room = Room(host=host, guest_can_pause=guest_can_pause,
+                            votes_to_skip=votes_to_skip)
                 room.save()
 
             return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
